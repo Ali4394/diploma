a117d115153ce6c165773aa21f503ddac6aa0da8
Мухаммед-Али Утеулиев <@itstep.loc>
2023-10-11 21:42:35 +0600

    pilot

---
+++ src/firebase/index.ts
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiBHWwt8mP_x1avBuGUQS2TXXAnK2t6To",
+  authDomain: "diploma-ca615.firebaseapp.com",
+  projectId: "diploma-ca615",
+  storageBucket: "diploma-ca615.appspot.com",
+  messagingSenderId: "156400392439",
+  appId: "1:156400392439:web:e0b131079cc908cc509bf2"
+};
+
+
+const app = initializeApp(firebaseConfig);
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
+import { initializeApp } from 'firebase/app'
+
+import router from './router'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/lara-dark-indigo/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCiBHWwt8mP_x1avBuGUQS2TXXAnK2t6To",
+    authDomain: "diploma-ca615.firebaseapp.com",
+    projectId: "diploma-ca615",
+    storageBucket: "diploma-ca615.appspot.com",
+    messagingSenderId: "156400392439",
+    appId: "1:156400392439:web:e0b131079cc908cc509bf2"
+  };
+
+
+  initializeApp(firebaseConfig)
 const app = createApp(App)
 
+app.use(PrimeVue)
 app.use(router)
 
 app.mount('#app')
