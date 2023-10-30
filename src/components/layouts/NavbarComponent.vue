9d8001c4b1a9b73a8a7a6ca5f86ba3ce02d7d9bc
Ali4394 <alitour356@gmail.com>
2023-10-30 08:49:06 +0600

    googleauth

--- src/components/layouts/NavbarComponent.vue
+++ src/components/layouts/NavbarComponent.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="side">
-    <Button v-if="!user" @click="googleRegister" label="Войти" icon="pi pi-sign-in" />
-    <Button v-else @click="googleLogout" label="Выйти" icon="pi pi-sign-out" />
+    <p-button v-if="!user" @click="googleRegister" label="Войти" icon="pi pi-sign-in" />
+    <p-button v-else @click="googleLogout" label="Выйти" icon="pi pi-sign-out" />
   </div>
 </template>
 
 <script setup lang="ts">
-import Button from 'primevue/button'
+import PButton from 'primevue/button'
 import { useUser } from '@/composables/useUser'
 
 const { user, googleRegister, googleLogout } = useUser()
 </script>
-<!-- import { useUser } from '@/composables/useUser.1'; -->
+import { useUser } from '@/composables/useUser.1';
